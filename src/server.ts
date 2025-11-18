7ca9fe61cb9d59bf090bc10d7dcb764b30f1e919
Leirisson souza <leirisson.santos@trf1.jus.br>
2025-11-18 12:29:13 -0400

    preparando para correção do bug,troquei o host do servidor

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { app } from './app'
 
 
 const PORT = Number(process.env.PORT) || 3333
-const HOST =  process.env.HOST || '172.19.5.127'
+const HOST =  process.env.HOST || '0.0.0.0'//'172.19.5.127'
 app.listen(PORT, HOST, () => {
     console.log(`Servidor rodando em ${HOST}:${PORT}`)
 })
