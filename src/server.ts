27d7f1b5405eb6a686fc2681b42d1515050ee741
Leirisson souza <leirisson.santos@trf1.jus.br>
2025-11-18 14:17:11 -0400

    conetando no banco de dados de debug

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import { app } from './app'
 
 
 const PORT = Number(process.env.PORT) || 3333
-const HOST =  process.env.HOST || '0.0.0.0'//'172.19.5.127'
+const HOST =  process.env.HOST || 'localhost'//'172.19.5.127'
 app.listen(PORT, HOST, () => {
-    console.log(`Servidor rodando em ${HOST}:${PORT}`)
+    console.log(`Servidor rodando em http://${HOST}:${PORT}`)
 })
 
